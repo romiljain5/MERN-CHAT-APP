f4fe3c0ca0100b4cbba0f291faf2f64d23d7134d
romiljain5 <jainromil56@gmail.com>
2023-08-03 10:16:12 +0530

    Add login and signup tabs in homepage using chakra UI prebuild components
    - Add background image

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import { Container, Box, Text } from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text
+          fontSize="4xl"
+          fontFamily="Open Sans"
+          color="black"
+          textAlign={"center"}
+        >
+          Talk-A-Tive
+        </Text>
+      </Box>
+      <Box
+        bg="white"
+        color="black"
+        w="100%"
+        borderRadius="lg"
+        p={4}
+        borderWidth="1px"
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width={"50%"}>Sign Up</Tab>
+          </TabList>
 
-export default HomePage
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default HomePage;
