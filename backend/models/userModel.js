1f94c8c80eb0b88fb4ff6d16ea3a4d2a0cd471e2
romiljain5 <jainromil56@gmail.com>
2023-08-05 22:57:16 +0530

    Create user authentication apis and logic
    - Encrypt/Decrypt password using bcryptjs
    - Define method for matchPassword in mongoose user instance
    - Use pre for calling function before save in mongoose
    - Use salt as 10 in bcryptjs for 10th level strong encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true , unique: true},
+    email: { type: String, required: true, unique: true },
+    password: {type: String, required: true},
     pic: {
       type: String,
       default:
@@ -14,6 +16,22 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next, options) {
+  // if(){
+  //     next();
+  // }
+//   options.validateModifiedOnly;
+//   next();
+    if (!this.isModified()) ( next());
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
