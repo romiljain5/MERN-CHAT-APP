09286a429db97bdf9860c519d59945b04fbe2d4c
romiljain5 <jainromil56@gmail.com>
2023-08-06 22:01:51 +0530

    Create chat api
    - Create chat api, We need to pass auth token and user id with post request to /api/chat
    - It will create group with user if not already their
    - If group is their we'll populate that group through populate method (which is mongoDB method)

---
+++ backend/controllers/chatController.js
@@ -0,0 +1,52 @@
+const asyncHandler = require("express-async-handler");
+const Chat = require("../models/chatModel");
+const User = require("../models/userModel");
+
+const accessChat = asyncHandler(async (req, res) => {
+  const { userId } = req.body;
+  if (!userId) {
+    console.log("userId param not sent with request");
+    return res.status(400);
+  }
+
+  //first we'll find group chat with user names
+  var isChat = await Chat.find({
+    isGroupChat: false,
+    $and: [
+      { users: { $elemMatch: { $eq: req.user._id } } },
+      { users: { $elemMatch: { $eq: userId } } },
+    ],
+  })
+    .populate("users", "-password")
+    .populate("latestMessage");
+
+  isChat = await User.populate(isChat, {
+    path: "latestMessage.sender",
+    select: "name pic email",
+  });
+
+  //if we get chat group then we send it
+  if (isChat.length > 0) {
+    res.send(isChat[0]);
+  } else {
+    //if we don't get any chat group with user name we create it
+    var chatData = {
+      chatName: "sender",
+      isGroupChat: false,
+      users: [req.user._id, userId],
+    };
+
+    try {
+      const createdChat = await Chat.create(chatData);
+      const FullChat = await Chat.findOne({ _id: createdChat._id }).populate(
+        "users",
+        "-password"
+      );
+      res.status(200).send(FullChat);
+    } catch (error) {
+      res.status(400);
+      throw new Error(error.message);
+    }
+  }
+});
+module.exports = { accessChat };
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
 const { errorHandler, notFound } = require("./middleware/errorMiddleware");
+const chatRoutes = require("./routes/chatRoutes");
 
 dotEnv.config();
 
@@ -14,19 +15,20 @@ const app = express();
 app.use(express.json()); //to accept json data by server
 app.use(cors());
 
-app.get("/route", (req, res) => {
-  res.send("api is running");
-});
+// app.get("/route", (req, res) => {
+//   res.send("api is running");
+// });
 
 app.use("/api/user", userRoutes);
-app.get("/api/chat", (req, res) => {
-  if (req.query.id) {
-    console.log(req.query.id);
-    const singleId = chats.find((chat) => chat._id === req.query.id);
-    res.send(singleId);
-  }
-  res.send(chats);
-});
+// app.get("/api/chat", (req, res) => {
+//   if (req.query.id) {
+//     console.log(req.query.id);
+//     const singleId = chats.find((chat) => chat._id === req.query.id);
+//     res.send(singleId);
+//   }
+//   res.send(chats);
+// });
+app.use("/api/chat", chatRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
